7e3450b051e3f773448a85c849ab6ac475898fbc
Juwon Lee <a8316010@gmail.com>
2026-06-09 12:43:02 +0900

    Movies Controller updates

--- dist/src/app.module.js
+++ dist/src/app.module.js
@@ -8,13 +8,14 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
+const movies_controller_1 = require("./movies/movies.controller");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
 exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
         imports: [],
-        controllers: [],
+        controllers: [movies_controller_1.MoviesController],
         providers: [],
     })
 ], AppModule);
---
+++ dist/src/movies/movies.controller.d.ts
@@ -0,0 +1,7 @@
+export declare class MoviesController {
+    getAll(): string;
+    getOne(movieId: string): string;
+    create(): string;
+    remove(movieId: string): string;
+    path(movieId: string): string;
+}
